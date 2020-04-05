7e0ae37e39f18874f8ddd0ffa8664a68078ff5ad
Jared S <jaredjames2020@gmail.com>
2020-04-05 18:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value){
   const newDriver = { ...driver };
-  
   newDriver[key] = value;
-  
   return newDriver;
+}
 
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver;
 }
