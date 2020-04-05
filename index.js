ddd009a2d284082a4d6654b8250c901444479bc5
Jared S <jaredjames2020@gmail.com>
2020-04-05 18:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your solution in this file!
 
 let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  const newDriver = updateDriverWithKeyAndValue(driver, key, value);
+  return newDriver
+}
