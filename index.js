4930797024303ca21363ecd9d88042105a122ea0
Jared S <jaredjames2020@gmail.com>
2020-04-05 19:02:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromDriverByKey(driver, key){
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key){
-  // const aDriver = (delete driver['key']);
-  // return aDriver;
+  const aDriver = (delete driver['key']);
+  return aDriver;
 }
